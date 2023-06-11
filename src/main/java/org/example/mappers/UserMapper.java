e94064a4e633bd6025280067f66aa454d4aece28
WeiMin-Coding <1772429411@qq.com>
2023-06-11 23:05:31 +0800

    Mybatis DML

---
+++ src/main/java/org/example/mappers/UserMapper.java
@@ -0,0 +1,5 @@
+package org.example.mappers;
+
+public interface UserMapper {
+    int insertUser();
+}
---
+++ src/test/java/MybatisTest.java
@@ -0,0 +1,21 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.example.mappers.UserMapper;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+public class MybatisTest {
+    @Test
+    public void testMybatis01() throws Exception {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory build = sqlSessionFactoryBuilder.build(resourceAsStream);
+        SqlSession sqlSession = build.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        int res = mapper.insertUser();
+        System.out.println("Inert: " + res);
+    }
+}
