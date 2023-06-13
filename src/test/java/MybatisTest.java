7556c2a7197b3478fda849c6bc316cab3d0544b4
WeiMin <1772429411@qq.com>
2023-06-13 20:07:36 +0800

    Test MySQL

--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -3,20 +3,56 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.example.mappers.UserMapper;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
 
 public class MybatisTest {
-    @Test
+    InputStream resourceAsStream;
+    SqlSessionFactoryBuilder sqlSessionFactoryBuilder;
+    SqlSessionFactory build;
+    SqlSession sqlSession;
+
+    @Before
+    public void before() throws Exception {
+        resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        build = sqlSessionFactoryBuilder.build(resourceAsStream);
+        sqlSession = build.openSession();
+    }
+
+//    @Test
     public void testMybatis01() throws Exception {
-        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
-        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory build = sqlSessionFactoryBuilder.build(resourceAsStream);
-        SqlSession sqlSession = build.openSession();
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         int res = mapper.insertUser();
         sqlSession.commit();
         System.out.println("Inert: " + res);
+    }
+
+    @Test
+    public void testInnodbReadWriteIOThread_1() {
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            mapper.insertUser();
+            sqlSession.commit();
+        }
+        long stopTime = System.currentTimeMillis();
+
+        System.out.println("10W Time: " + (stopTime - startTime));
+    }
+
+    @Test
+    public void testInnodbReadWriteIOThread_16() {
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            mapper.insertUser();
+            sqlSession.commit();
+        }
+        long stopTime = System.currentTimeMillis();
+
+        System.out.println("10W Time: " + (stopTime - startTime));
     }
 }
